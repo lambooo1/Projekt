816d3435a7fca40bacd517fe9b2512ad783153af
lambooo1 <logendranl@bzz.ch>
2022-10-05 21:38:42 +0200

    feat: add listTask (not complete) and createCell

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
+const createCell = (cellText) => {
+    const cell = document.createElement("td");
+    cell.innerText = cellText;
+    return cell;
+  }
+
+  const listTask = (task) => {
+    const display = document.getElementById("display");
+    const tableRow = document.createElement("tr");
+
+    tableRow.append(
+      createCell(task.id), 
+      createCell(task.completed), 
+      createCell(task.title), 
+    );
+    display.appendChild(tableRow); 
+    }
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    const listAllExercises = document.getElementById("listAllExercises")
+    const taskId = document.getElementById("taskId")
     const addNewExercise = document.getElementById("addNewExercise")
     const updateExercise = document.getElementById("updateExercise")
     const deleteExercise = document.getElementById("deleteExercise") 
 
-    listAllExercises.addEventListener("click", () => {
+    taskId.addEventListener("click", () => {
         alert("alles anzeigen")
+        //event.preventDefault(); 
+        //const taskIdData = new FormData(taskId);
+
+        fetch(`http://127.0.0.1:3002/m294-lb-backend-main/m294-lb-backend-main/index.html#taskTaskIdGet`)  
+            .then((response) => {
+                return response.json(); 
+            }) 
+            .then((data) => {
+                listTask(data); 
+            }) 
     }); 
+
     addNewExercise.addEventListener("click", () => {
         alert("neu hinzufügen")
     });
+
     updateExercise.addEventListener("click", () => {
         alert("updaten")
     });
+
     deleteExercise.addEventListener("click", () => {
         alert("löschen")
     });
