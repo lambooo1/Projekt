a016d8e056c0de9da36b288bf75095cdb053bb3f
lambooo1 <logendranl@bzz.ch>
2022-10-06 16:21:38 +0200

    feat: add function delete

--- script.js
+++ script.js
@@ -3,17 +3,30 @@ const createCell = (cellText) => {
     cell.innerText = cellText;
     return cell;
   }
+/*
+const createButton = () => {
+    const cellButton = document.createElement("button");
+    cell.innerText = "Löschen"
+    return cellButton
+}
+*/
 
 function renderTasks(tasks){
     const tableBody = document.querySelector("tbody"); 
     tasks.forEach((task) => { 
         const tableRow = document.createElement("tr")  
-        const deleteButton = document.createElement("button")      
-        deleteButton.innerText = "löschen"  
+        const deleteButton = document.createElement("button")
+        deleteButton.innerText = "Löschen"
+        
+        deleteButton.addEventListener("click", () => {
+            deleteTask(task.id); 
+        })
+
 
         tableRow.append(createCell(task.id), createCell(task.title), createCell(task.completed))
         tableBody.appendChild(tableRow)
-        tableRow.appendChild(deleteButton) 
+        tableRow.appendChild(deleteButton)  
+        
         }
         )}
 
@@ -25,7 +38,6 @@ function indextasks(){
     .then((data) => {
         return renderTasks(data)}); 
 }
-
 
 function createTask() {
     const addNewExercise = document.getElementById("addNewExercise");
@@ -46,56 +58,46 @@ function createTask() {
      indextasks.id
 }
 
-function deleteTask() {
-    const allTaskId = document.querySelector("tbody") 
-    const deletedTask = {id: allTaskId.value}
-    fetch(`http://localhost:3000/tasks`, {
-    method: 'DELETE',
-    headers: {
-   'Content-Type':'application/json'
-    },
-    body: JSON.stringify(deletedTask)
+function deleteTask(id) {
+    fetch(`http://localhost:3000/task/${id}`,{
+    method: 'DELETE'
     })
-    .then((response) => response.json())
-    .then((deletedTask) => {
-    console.log(deletedTask); 
-    })
+    location.reload(); 
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    
+document.addEventListener("DOMContentLoaded", () => { 
+
+
+
     const submit = document.getElementById("submit")
     const taskId = document.getElementById("taskId")
-    const deleteId = document.getElementById("deleteId")  
 
-    const newExerciseForm = document.getElementById("newExerciseForm")
     const updateExercise = document.getElementById("updateExercise")
-    const deleteExercise = document.getElementById("deleteExercise") 
 
     taskId.addEventListener("click", () => {
         /*
-        while(tableId.rows.length>0)
-        {
-            tableId.deleteRow(0);
-        }
-
+        while(tableId.rows.length>0) 
+            {tableId.deleteRow(0);}
         */
-        indextasks(); 
+        indextasks();  
+        
     })
 
     submit.addEventListener("submit", () => {
         createTask(); 
     }); 
-
-    deleteId.addEventListener("submit", () => {
-        deleteTask(); 
-    });
+    
+    /*
+    const deleteButton2 = document.getElementsByTagName("button")
+    deleteButton2.addEventListener("click", () => {    
+    if (deleteButton2.innerText == "Löschen"){
+        deleteTask(task.id)} 
+    else {console.log()} 
+    }) 
+    */
 
     updateExercise.addEventListener("click", () => {
         alert("updaten")
     });
 
-    deleteExercise.addEventListener("click", () => {
-        
-    });
 });
