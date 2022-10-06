dabed17f3b024c4d708aaf2be14a254604ec1b73
lambooo1 <logendranl@bzz.ch>
2022-10-06 14:16:43 +0200

    feat: add functions showTasks and addTask

--- script.js
+++ script.js
@@ -4,41 +4,91 @@ const createCell = (cellText) => {
     return cell;
   }
 
-  const listTask = (task) => {
-    const display = document.getElementById("display");
-    const tableRow = document.createElement("tr");
+function renderTasks(tasks){
+    const tableBody = document.querySelector("tbody"); 
+    tasks.forEach((task) => { 
+        const tableRow = document.createElement("tr")  
+        const deleteButton = document.createElement("button")      
+        deleteButton.innerText = "löschen"  
 
-    tableRow.append(
-      createCell(task.id), 
-      createCell(task.completed), 
-      createCell(task.title), 
-    );
-    display.appendChild(tableRow); 
-    }
+        tableRow.append(createCell(task.id), createCell(task.title), createCell(task.completed))
+        tableBody.appendChild(tableRow)
+        tableRow.appendChild(deleteButton) 
+        }
+        )}
 
+function indextasks(){
+    fetch(`http://localhost:3000/tasks`) 
+    .then((response) => {
+        return response.json()})
+
+    .then((data) => {
+        return renderTasks(data)}); 
+}
+
+
+function createTask() {
+    const addNewExercise = document.getElementById("addNewExercise");
+    const task = {title: addNewExercise.value} 
+    fetch(`http://localhost:3000/tasks`, {
+    method: 'POST',
+    headers: {
+   'Content-Type':'application/json'
+    },
+    body: JSON.stringify(task)
+    })
+    .then((response) => response.json())
+    .then((task) => {
+    console.log(task);
+    })
+     indextasks(); 
+     //alert(`${task} wurde hinzugefügt.`)
+     indextasks.id
+}
+
+function deleteTask() {
+    const allTaskId = document.querySelector("tbody") 
+    const deletedTask = {id: allTaskId.value}
+    fetch(`http://localhost:3000/tasks`, {
+    method: 'DELETE',
+    headers: {
+   'Content-Type':'application/json'
+    },
+    body: JSON.stringify(deletedTask)
+    })
+    .then((response) => response.json())
+    .then((deletedTask) => {
+    console.log(deletedTask); 
+    })
+}
 
 document.addEventListener("DOMContentLoaded", () => {
+    
+    const submit = document.getElementById("submit")
     const taskId = document.getElementById("taskId")
-    const addNewExercise = document.getElementById("addNewExercise")
+    const deleteId = document.getElementById("deleteId")  
+
+    const newExerciseForm = document.getElementById("newExerciseForm")
     const updateExercise = document.getElementById("updateExercise")
     const deleteExercise = document.getElementById("deleteExercise") 
 
     taskId.addEventListener("click", () => {
-        alert("alles anzeigen")
-        //event.preventDefault(); 
-        //const taskIdData = new FormData(taskId);
+        /*
+        while(tableId.rows.length>0)
+        {
+            tableId.deleteRow(0);
+        }
 
-        fetch(`http://127.0.0.1:3002/m294-lb-backend-main/m294-lb-backend-main/index.html#taskTaskIdGet`)  
-            .then((response) => {
-                return response.json(); 
-            }) 
-            .then((data) => {
-                listTask(data); 
-            }) 
+        */
+        indextasks(); 
+    })
+
+    submit.addEventListener("submit", () => {
+        createTask(); 
     }); 
 
-    addNewExercise.addEventListener("click", () => {
-        alert("neu hinzufügen")
+    deleteId.addEventListener("submit", () => {
+        deleteTask(); 
     });
 
     updateExercise.addEventListener("click", () => {
@@ -46,6 +96,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     deleteExercise.addEventListener("click", () => {
-        alert("löschen")
+        
     });
-})
+});
